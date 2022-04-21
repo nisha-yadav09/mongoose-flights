f956a88a33bd3050073b75e05936f7d6a36a0559
nisha09 <nisha1947@gmail.com>
2022-04-21 06:14:05 +0530

    Added part III functionalities

--- mongoose-flights/controllers/tickets.js
+++ mongoose-flights/controllers/tickets.js
@@ -2,19 +2,19 @@ const Ticket = require('../models/ticket');
 const Flight = require('../models/flight');
 
 module.exports = {
-  new: newTicket,
-  create
+    new: newTicket,
+    create
 };
 
 function create(req, res) {
-  const s = req.body.born;
-  req.body.born = `${s.substr(5, 2)}-${s.substr(8, 2)}-${s.substr(0, 4)}`;
-  Ticket.create(req.body, function (err, ticket) {
-    res.redirect('/tickets/new');
-  });
-}
+    req.body.flight = req.params.id;
+    Ticket.create(req.body, function (err, ticket) {
+        res.redirect(`/flights/${req.params.id}`);
+    });
+};
+
 
 function newTicket(req, res) {
     let flightId = req.params.id;
     res.render('tickets/new', { flightId });
-  };
+};
--- mongoose-flights/routes/tickets.js
+++ mongoose-flights/routes/tickets.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var ticketController = require('../controllers/tickets'); 
 
 router.get('/flights/:id/tickets/new', ticketController.new);
-router.post('/', ticketController.create);
+router.post('/flights/:id/tickets', ticketController.create); 
 
 module.exports = router;
